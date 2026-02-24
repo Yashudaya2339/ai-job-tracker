4ca04719976b09e87b9a8f266f4a02c61b2a1ffa
Yashudaya2339 <yashudaya012339@gmail.com>
2026-02-24 19:55:00 +0530

    Day 1: Setup React frontend + Express backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());  //lets us read JSON from requests
+
+//Test route
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Backend is alive!'
+    });
+});
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
